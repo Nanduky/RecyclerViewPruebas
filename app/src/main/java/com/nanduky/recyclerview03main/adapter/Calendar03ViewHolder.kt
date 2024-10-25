0ee0d5034e4f6b0ed13fd22798786d68b2342e10
Nanduky <nanduky2@gmail.com>
2024-10-25 22:37:32 +0200

    Add Adapter

---
+++ app/src/main/java/com/nanduky/recyclerview03main/adapter/Calendar03ViewHolder.kt
@@ -0,0 +1,4 @@
+package com.nanduky.recyclerview03main.adapter
+
+class Calendar03ViewHolder {
+}
---
+++ app/src/main/java/com/nanduky/recyclerview03main/clases/Calendar03DC.kt
@@ -0,0 +1,7 @@
+package com.nanduky.recyclerview03main.clases
+
+data class Calendar03DC(
+    val data :String,
+    val place: String,
+    val doctor: String
+)
